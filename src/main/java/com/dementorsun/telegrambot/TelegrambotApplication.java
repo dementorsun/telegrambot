790b7f8ea4134c55be0adc2d9eff4bb74f52bd33
dementorsun <dementorsun@gmail.com>
2021-07-25 23:24:40 +0300

    'init_commit'

---
+++ src/main/java/com/dementorsun/telegrambot/TelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.dementorsun.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambotApplication.class, args);
+	}
+
+}

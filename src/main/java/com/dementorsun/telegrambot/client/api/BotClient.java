2e45ab5460ca0c3f853fd098cdb1a1a1f6803fcd
dementorsun <dementorsun@gmail.com>
2022-06-21 21:47:10 +0300

    delete_anime_from_tmdb_tv_show_endpoint

--- src/main/java/com/dementorsun/telegrambot/client/api/BotClient.java
+++ src/main/java/com/dementorsun/telegrambot/client/api/BotClient.java
@@ -41,14 +41,14 @@ public class BotClient {
     }
 
     public String getRandomMovie() {
-        final int totalPages = 61;
+        final int totalPages = 62;
         int page = getRandomResponseItem(totalPages);
 
         return tmdbApiClient.getRandomMovie(page);
     }
 
     public String getRandomTvShow() {
-        final int totalPages = 66;
+        final int totalPages = 67;
         int page = getRandomResponseItem(totalPages);
 
         return tmdbApiClient.getRandomTvShow(page);
